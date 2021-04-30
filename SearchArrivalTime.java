332bd30b18bc1ffce0cc6130a70791561c59d254
Davideneher <60857414+Davideneher@users.noreply.github.com>
2021-04-30 15:04:54 +0300

    Update SearchArrivalTime.java

--- SearchArrivalTime.java
+++ SearchArrivalTime.java
@@ -4,68 +4,70 @@ import java.text.*;
 
 public class SearchArrivalTime {
 	
+	public static final String MAX_TIME = "24:00:00";
+
 	public static void main(String[] args) {
 		System.out.println("Enter arrival time (hh:mm:ss)");
-		Scanner input = new Scanner( System.in );
+		Scanner input = new Scanner(System.in);
 		String item = input.next();
-		getlist(item);
-	}
 
-    public static final String MAX_TIME = "24:00:00";
-	
-	public static ArrayList<String> parseFile(String input){
-		
-        ArrayList<String> trips = new ArrayList<String>();
-        ArrayList<String> tripIDs = new ArrayList<String>();
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-
-		try{
-			Date maxTime = sdf.parse(MAX_TIME);
-			BufferedReader br = new BufferedReader(new FileReader("inputs/stop_times.txt"));
-			br.readLine();
-			String line;
-	        while ((line = br.readLine()) != null) {
-	        	if (!line.isEmpty()) {
-	            	String[] temp = line.split(",");
-	            	Date time = sdf.parse(temp[1]);
-	            	if (time.getTime() < maxTime.getTime()) {
-	            		trips.add(line);
-	            	}
-	        	}
-            }
-            br.close();
-        } catch (Exception e){
-            System.out.print(e);
-        }
-        
-        for (int i = 0; i<trips.size(); i++) {
-			String line = trips.get(i);
-			String[] t = line.split(",");
-			try {
-				Date time = sdf.parse(t[1]);
-				Date inputTime = sdf.parse(input);
-				if (time.getTime() == inputTime.getTime()) {
-					tripIDs.add(line);
-				}
-			} catch (Exception e){
-	            System.out.print(e);
-	        }
-		}
-        //tripIDs.sort(Comparator.comparing(Double::parseDouble));
-        return tripIDs;
-	}
-		
-	public static void getlist(String searchItem){
-		ArrayList<String> list = parseFile(searchItem);
+		ArrayList<String> list = parseFile(item);
 		if (!list.isEmpty()) {
-			System.out.println(list.size() + " trips match your search:");
-			for(int i=0; i < list.size(); i++){
-	            System.out.println( list.get(i) );
-	        }
-		}
-		else {
+			for (int i = 0; i < list.size(); i++) {
+				System.out.println(list.get(i));
+			}
+		} else {
 			System.out.println("There are no trips that match your search.");
 		}
 	}
-	
+
+	public static ArrayList<String> parseFile(String input) {
+
+		ArrayList<String> trips = new ArrayList<String>();
+		ArrayList<String> searchedTrips = new ArrayList<String>();
+		List<Double> id = new ArrayList<>(Arrays.asList());
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+
+		try {
+			String line;
+			
+			BufferedReader br = new BufferedReader(new FileReader("inputs/stop_times.txt"));
+			br.readLine();
+			while ((line = br.readLine()) != null) {
+				if (!line.isEmpty()) {
+						trips.add(line);
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.print(e);
+		}
+
+		for (int i = 0; i < trips.size(); i++) {
+			String line = trips.get(i);
+			String[] t = line.split(",");
+
+			try {
+				Date maxTime = sdf.parse(MAX_TIME);
+				Date time = sdf.parse(t[1]);
+				double ids = Double.parseDouble(t[0]);
+				Date inputTime = sdf.parse(input);
+				if (time.getTime() == inputTime.getTime() && time.getTime() < maxTime.getTime()	) {
+					try {
+						id.add(ids);
+						searchedTrips.add(line);
+					} catch (NumberFormatException ex) { // handle your exception
+						System.out.print("");
+					}
+
+				}
+			} catch (Exception e) {
+				System.out.print(e);
+			}
+		}
+		searchedTrips.sort(Comparator.naturalOrder());
+		return searchedTrips;
+	}
+
 }
