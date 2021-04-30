409a9e9c75d100f39e219301dc8426bc914091c8
Dermot <contact@dermotobrien.me>
2021-04-30 21:33:52 +0100

    Remove bugged code :o:

--- SearchArrivalTime.java
+++ SearchArrivalTime.java
@@ -11,20 +11,20 @@ public class SearchArrivalTime {
 	
 	public static final String MAX_TIME = "24:00:00";
 
-	public static void main(String[] args) {
-		System.out.println("Enter arrival time (hh:mm:ss)");
-		Scanner input = new Scanner(System.in);
-		String item = input.next();
+	// public static void main(String[] args) {
+	// 	System.out.println("Enter arrival time (hh:mm:ss)");
+	// 	Scanner input = new Scanner(System.in);
+	// 	String item = input.next();
 
-		ArrayList<String> list = parseFile(item);
-		if (!list.isEmpty()) {
-			for (int i = 0; i < list.size(); i++) {
-				System.out.println(list.get(i));
-			}
-		} else {
-			System.out.println("There are no trips that match your search.");
-		}
-	}
+	// 	ArrayList<String> list = parseFile(item);
+	// 	if (!list.isEmpty()) {
+	// 		for (int i = 0; i < list.size(); i++) {
+	// 			System.out.println(list.get(i));
+	// 		}
+	// 	} else {
+	// 		System.out.println("There are no trips that match your search.");
+	// 	}
+	// }
 
 	public static ArrayList<String> parseFile(String input) {
 
@@ -37,7 +37,7 @@ public class SearchArrivalTime {
 		try {
 			String line;
 			
-			BufferedReader br = new BufferedReader(new FileReader("inputs/stop_times.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("./inputs/stop_times.txt"));
 			br.readLine();
 			while ((line = br.readLine()) != null) {
 				if (!line.isEmpty()) {
@@ -59,13 +59,14 @@ public class SearchArrivalTime {
 				double ids = Double.parseDouble(t[0]);
 				
 
-				String pattern = "[0-24]:[0-59]:[0-59]";
-				Pattern r = Pattern.compile(pattern);
-				Matcher m = r.matcher(line);
-				if(!m.find()) { 
-					System.out.println("Invalid date");
-					break;
-				} 
+				// String pattern = "[0-24]:[0-59]:[0-59]";
+				// Pattern r = Pattern.compile(pattern);
+				// Matcher m = r.matcher(input);
+				// System.out.println(m.matches());
+				// if(!m.find()) { 
+				// 	System.out.println("Invalid date");
+				// 	break;
+				// } 
 
 				Date inputTime = sdf.parse(input);
 				if (time.getTime() == inputTime.getTime() && time.getTime() < maxTime.getTime()	) {
