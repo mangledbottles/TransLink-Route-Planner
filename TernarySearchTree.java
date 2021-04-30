00355c9ef383512704428e4c21f781d684ac0305
Dermot <contact@dermotobrien.me>
2021-04-30 11:30:17 +0100

    Read in file

--- TernarySearchTree.java
+++ TernarySearchTree.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class TernarySearchTree {
     public static void main(String[] args) {
-        System.out.println('Ternary Search Tree');
+    }
+
+    TernarySearchTree(String fileName) {
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+        } catch(Exception error) {
+            System.out.println("Its broken.. : " + error.toString());
+        }
     }
 }
