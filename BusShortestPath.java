238fb1d9f493742dca87b9cd7397147af8b53b33
barryos112 <78805659+barryos112@users.noreply.github.com>
2021-04-30 12:11:00 +0100

    Added shortest path ouput

--- BusShortestPath.java
+++ BusShortestPath.java
@@ -7,13 +7,13 @@ public class BusShortestPath {
 	private String stop_times;
 	private String transfers;
 	private double Matrix[][] = new double[15000][15000];
-	
-	
+
+
 
 	BusShortestPath(String stop_times, String transfers) {
 		this.stop_times = stop_times;
 		this.transfers = transfers;
-		
+
 
 		try {
 			makeMatrix();
@@ -21,9 +21,9 @@ public class BusShortestPath {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void makeMatrix() throws FileNotFoundException {
-		
+
 		for(int i = 0; i < Matrix.length; i++) {
 			for(int j = 0; j < Matrix[i].length; j++) {
 				if(i != j) {
@@ -34,35 +34,35 @@ public class BusShortestPath {
 				}
 			}
 		}
-		//read in stop times file 
-		
+		//read in stop times file
+
 		File stopTimesFile = new File(stop_times);
 		Scanner fileScan = new Scanner(stopTimesFile);
 		Scanner lineScan = null;
 		fileScan.nextLine();
-		
+
 		int from = 0;
 		int to = 0;
-		int prevRouteId = 0; 
+		int prevRouteId = 0;
 		int routeId = 0;
-		
-		//use different weights based on which file you're using 
+
+		//use different weights based on which file you're using
 		double weight;
 		String currentLine;
-		
-		
+
+
 		weight = 1;
 		while(fileScan.hasNextLine()) {
 			currentLine = fileScan.nextLine();
 			lineScan = new Scanner(currentLine);
 			lineScan.useDelimiter(",");
-			
+
 			prevRouteId = routeId;
 			routeId = lineScan.nextInt();
-		
+
 ////			lineScan.next();
 ////			lineScan.next();
-			
+
 			from = to;
 			to = lineScan.nextInt();
 			if(prevRouteId == routeId) {
@@ -71,34 +71,34 @@ public class BusShortestPath {
 			lineScan.close();
 		}
 		fileScan.close();
-		
-		int transferType; 
+
+		int transferType;
 		double minTime;
 		double divisor = 100;
-		
+
 		try {
 			File transfersFile = new File(transfers);
 			fileScan = new Scanner(transfersFile);
-			
+
 			fileScan.nextLine();
 		}
 		 catch(Exception error) {
 	            System.out.println("Its broken.. : " + error.toString());
 	        }
-		
+
 //		File transfersFile = new File(transfers);
-		
-		
-		
+
+
+
 		while(fileScan.hasNextLine()) {
 			currentLine = fileScan.nextLine();
 			fileScan = new Scanner(currentLine);
 			fileScan.useDelimiter(",");
-			
+
 			from = fileScan.nextInt();
 			to = fileScan.nextInt();
 			transferType = fileScan.nextInt();
-			
+
 			if(transferType == 0) {
 				Matrix[from][to] = 2;
 			}
@@ -109,27 +109,27 @@ public class BusShortestPath {
 			lineScan.close();
 		}
 		fileScan.close();
-	
+
 	}
-	
-	
+
+
     private void relaxEdge(int from, int to, double[] distTo, int[] edgeTo) {
     	if(distTo[to] > distTo[from] + Matrix[from][to]) {
     		distTo[to] = distTo[from] + Matrix[from][to];
     		edgeTo[to] = from;
     	}
     }
-	
+
 	public String findShortDistance(int from, int to){
-		
+
 		if(from == to) {
 			return "" + Matrix[from][to] + " through " + from;
 		}
-		
+
 		int visited[] = new int[Matrix.length];
     	double distTo[] = new double[Matrix.length];
     	int edgeTo[] = new int[Matrix.length];
-    	
+
     	//set all but starting node to infinity
     	for(int i = 0; i < distTo.length; i++) {
     		if(i != from)
@@ -137,13 +137,13 @@ public class BusShortestPath {
     			distTo[i] = Double.POSITIVE_INFINITY;
     		}
     	}
-    	
+
     //Dijkstra's Algorithm
     	visited[from] = 1;
-    	distTo[from] = 0; 
+    	distTo[from] = 0;
     	int currentNode = from;
-    	int totalNodesVisited = 0;
-    	while(totalNodesVisited < distTo.length)
+    	int VisitedNodes = 0;
+    	while(VisitedNodes < distTo.length)
     	{
     		//relax the edges pointing from the current node then set it as visited
     		for(int i = 0; i < Matrix[currentNode].length; i ++) {
@@ -152,7 +152,7 @@ public class BusShortestPath {
         		}
     		}
     		visited[currentNode] = 1;
-    		
+
     		//pick node w shortest distance to relax
     		double shortestDist = Integer.MAX_VALUE;
     		for(int i = 0; i < distTo.length; i++) {
@@ -161,25 +161,25 @@ public class BusShortestPath {
     				shortestDist = distTo[i];
     			}
     		}
-    		totalNodesVisited++;
+    		VisitedNodes++;
     	}
-    	
+
     	//build the path we took through the graph
     	if(distTo[to] == Double.POSITIVE_INFINITY) {
     		return "not existent";
     	}
-    	
-    	int u = from;
-    	int v = to;
-    	String trace = "";
-    	while(v != u) {
-    		trace =  "->" + edgeTo[v] + trace;
-    		v = edgeTo[v];
+
+    	int x = from;
+    	int y = to;
+    	String path = "";
+    	while(x != y) {
+    		path =  "------>" + edgeTo[x] + path;
+    		x = edgeTo[x];
     	}
-    	trace = trace + "->" + to;
-    	
-    	return distTo[to] + " through " + trace;
+    	path = path + "------>" + to;
+
+    	return distTo[to] + " through " + path;
     }
-	
+
 
 }
