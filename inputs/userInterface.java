1e2fb0234080c7569a2fe787ef35fe279167682b
ben-vaughan <56156727+ben-vaughan@users.noreply.github.com>
2021-04-30 09:58:58 +0100

    Choice functionality

--- inputs/userInterface.java
+++ inputs/userInterface.java
@@ -18,6 +18,7 @@ public class userInterface {
 
     String stringIn = input.next();
     int choice = 0;
+
     try {
       choice = Integer.parseInt(stringIn);
     }
@@ -25,6 +26,59 @@ public class userInterface {
       System.out.println("Invalid input provided.");
     }
 
+    if(choice == 0) {
+      return false;
+    }
+
+    else {
+      if(choice >= 1 && choice <= 3) {
+
+        // Option 1: Finding the shortest path between two bus stops //
+        if (choice == 1) {
+          int stopA;
+          int stopB;
+
+          System.out.println("Enter the first stop number: ");
+          if(input.hasNextInt()) {
+            stopA = input.nextInt();
+          }
+          else {
+            System.out.println("Invalid stop number.");
+          }
+
+          System.out.println("Enter the second stop number: ");
+          if(input.hasNextInt()) {
+            stopB = input.nextInt();
+          }
+          else {
+            System.out.println("Invalid stop number.");
+          }
+
+          // findShortestPath(stop1, stop2, graph)
+        }
+
+        // Option 2: Finding information regarding a specific bus stop //
+        else if (choice == 2) {
+          System.out.println("Enter the stop name: ");
+          String stopIn = input.next().toUpperCase();
+          // searchBusStops(stopIn);
+        }
+
+        // Option 3: Finding all trips with a desired arrival time //
+        else if (choice == 3) {
+          System.out.println("Enter your arrival time [hh:mm:ss]: ");
+          String timeIn = input.nextLine();
+
+          findArrivalTime(timeIn);
+
+          // TODO: error check time format
+        }
+        else {
+          System.out.println("Invalid input provided.");
+        }
+      }
+    }
+    return true;
   }
 
 
